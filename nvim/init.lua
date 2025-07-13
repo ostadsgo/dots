4b63b7e6f3a07562f8e95d0c3d907443d171b71a
Saeed Gholami <saeed.ghollami@gmail.com>
2025-07-13 22:21:45 +0330

    Add ignore pattern for fzf-lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,40 +2,40 @@
 -- options
 -- -------------------
 -- File handling
-vim.opt.backup = false          
-vim.opt.writebackup = false     
-vim.opt.swapfile = false        
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.swapfile = false
 
 -- UI/Display
-vim.opt.termguicolors = true    
-vim.opt.number = true           
-vim.opt.relativenumber = true   
-vim.opt.showcmd = false         
-vim.opt.wrap = false            
-vim.opt.guicursor = ""          
-vim.opt.syntax = "on"          
+vim.opt.termguicolors = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.showcmd = false
+vim.opt.wrap = false
+vim.opt.guicursor = ""
+vim.opt.syntax = "on"
 
 -- Search
-vim.opt.hlsearch = false        
-vim.opt.incsearch = true        
-vim.opt.ignorecase = true       
-vim.opt.smartcase = true        
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 -- Indentation
-vim.opt.expandtab = true        
-vim.opt.tabstop = 2             
-vim.opt.shiftwidth = 2          
-vim.opt.softtabstop = 2         
-vim.opt.autoindent = true       
-vim.opt.smartindent = true      
-vim.opt.cindent = true          
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.cindent = true
 
 -- Completion
 vim.opt.completeopt = { "menuone", "noselect" }
 
 -- Window management
-vim.opt.splitbelow = true       
-vim.opt.splitright = true      
+vim.opt.splitbelow = true
+vim.opt.splitright = true
 
 -- color
 vim.opt.background = "dark"
@@ -58,8 +58,8 @@ vim.g.maplocalleader = "\\"
 -- Resize window
 vim.keymap.set("n", "<C-Down>", ":resize -2<CR>")
 vim.keymap.set("n", "<C-Left>", ":vertical resize +2<CR>")
-vim.keymap.set("n",  "<C-Right>", ":vertical resize -2<CR>")
-vim.keymap.set("n",  "<C-Up>", ":resize +2<CR>")
+vim.keymap.set("n", "<C-Right>", ":vertical resize -2<CR>")
+vim.keymap.set("n", "<C-Up>", ":resize +2<CR>")
 -- create empty line
 vim.keymap.set("n", "<A-j>", "o<ESC>")
 vim.keymap.set("n", "<A-k>", "O<ESC>")
@@ -79,7 +79,7 @@ vim.keymap.set("n", "<Leader>a", "ggVG")
 vim.keymap.set("n", "<Leader>W", ":wa<CR>")
 vim.keymap.set("n", "<Leader>e", ":Exp<CR>")
 vim.keymap.set("n", "<Leader>q", ":bd<CR>")
-vim.keymap.set("n", "<Leader>r", ":source $MYVIMRC<CR>" )
+vim.keymap.set("n", "<Leader>r", ":source $MYVIMRC<CR>")
 vim.keymap.set("n", "<Leader>w", ":w<CR>")
 -- paste from sys clipboard
 vim.keymap.set("n", "<Leader>p", '"+p')
@@ -88,14 +88,8 @@ vim.keymap.set("n", "<Leader>P", '"+P')
 vim.keymap.set("n", "<leader>d", '"_d')
 -- keep cursor pos after yank
 vim.keymap.set("n", "<Leader>y", '"+ygv<ESC>')
--- Exec lua code 
+-- Exec lua code
 vim.keymap.set("n", "<Leader>x", ":.lua<CR>")
--- Mini picker
-vim.keymap.set("n", "<Leader>ff", ":lua require('fzf-lua').files()<CR>" )
-vim.keymap.set("n", "<Leader>fb", ":lua require('fzf-lua').buffers()<CR>" )
-vim.keymap.set("n", "<Leader>fg", ":lua require('fzf-lua').grep()<CR>" )
-vim.keymap.set("n", "<Leader>fs", ":lua require('fzf-lua').live_grep()<CR>" )
-
 
 -- Insert
 vim.keymap.set("i", "jk", "<Esc>")
@@ -135,7 +129,6 @@ vim.keymap.set("c", "<A-f>", "<Right>")
 vim.keymap.set("c", "<A-n>", "<Down>")
 vim.keymap.set("c", "<A-p>", "<Up>")
 
-
 -- -----------------------
 -- LAZY PACKAGE MANAGER
 -- -----------------------
@@ -155,34 +148,66 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-
 -- -----------------------
 -- PLUGINS
 -- -----------------------
 -- List of Plugins
 local plugins = {
   -- fuzzy finder
-  { "ibhagwan/fzf-lua", 
-    opts = { 
-      winopts = { row=1, col=0, height=.4, width=1, preview={ hidden=true } },
+  {
+    "ibhagwan/fzf-lua",
+    opts = {
+      winopts = { row = 1, col = 0, height = 0.4, width = 1, preview = { hidden = true } },
+      files = {
+        find_opts = [[-type f \! -path '*/.git/*' \! -path '*/.venv/*' \! -path '*/__pycache__/*' \! -path '*/node_modules/*' ]],
+      },
+    },
+    keys = {
+      { "<Leader>ff", ":lua require('fzf-lua').files()<CR>" },
+      { "<Leader>fb", ":lua require('fzf-lua').buffers()<CR>" },
+      { "<Leader>fg", ":lua require('fzf-lua').grep()<CR>" },
+      { "<Leader>fs", ":lua require('fzf-lua').live_grep()<CR>" },
     },
   },
   -- lsp
   { "neovim/nvim-lspconfig" },
   -- colorscheme
-  {'nyoom-engineering/oxocarbon.nvim', 
+  {
+    "nyoom-engineering/oxocarbon.nvim",
+    priority = 1000,
     config = function()
-      vim.cmd('colorscheme oxocarbon') 
-      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-      vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-    end },
-}
-require("lazy").setup({ spec = plugins, checker = { enabled = false }, })
+      vim.cmd("colorscheme oxocarbon")
+      -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+      -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+      -- vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+    end,
+  },
+  {
+    "folke/tokyonight.nvim",
+    priority = 1000,
+    config = function()
+      vim.cmd("colorscheme oxocarbon")
+    end,
+  },
+  -- treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "master",
+    lazy = false,
+    build = ":TSUpdate",
+    main = "nvim-treesitter.configs",
+    opts = {
+      ensure_installed = { "python", "javascript", "html", "css", "lua", "go" },
+      highlight = { enable = true },
+      indent = { enable = true },
+    },
+  },
+} -- end of list of plugins
+require("lazy").setup({ spec = plugins, checker = { enabled = false } })
 
 -- -----------------------
 -- LSP
 -- -----------------------
-vim.lsp.enable({ 'lua_ls', 'pyright' })
+vim.lsp.enable({ "lua_ls", "pyright" })
 vim.diagnostic.config({ virtual_text = true })
 vim.lsp.set_log_level("debug")
