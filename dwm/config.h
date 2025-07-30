0cc5afb5d5235c7fd904c1a506f9cf5323adf6b0
Saeed Gholami <saeed.ghollami@gmail.com>
2025-07-31 02:18:21 +0330

    Add alpha patch to st.

--- dwm/config.h
+++ dwm/config.h
@@ -30,7 +30,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-  { "firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
+  { "Firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
   { "Tk",       NULL,       NULL,       0,            1,           -1 },
   { "app.py",   NULL,       NULL,       0,            1,           -1 },
 };
