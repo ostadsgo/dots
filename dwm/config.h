5fb2be77d4fe1596a18ab9047bd7a43b0c09bb05
Saeed Gholami <saeed.ghollami@gmail.com>
2025-08-21 01:47:35 +0330

    Add bluthooth packages.

--- dwm/config.h
+++ dwm/config.h
@@ -25,8 +25,8 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	/* class    instance    title       tags mask     isfloating   monitor */
+  { "Gimp",     NULL,       NULL,       0,            1,           -1 },
   { "Firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
   { "Tk",       NULL,       NULL,       0,            1,           -1 },
   { "app.py",   NULL,       NULL,       0,            1,           -1 },
@@ -59,7 +59,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_black, "-nf", col_gray, "-sb", col_black, "-sf", col_white, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_black, "-nf", col_gray, "-sb", col_red, "-sf", col_white, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *explorer[] = {"pcmanfm", NULL};
 static const char *emacs[] = {"emacs", NULL};
@@ -69,9 +69,9 @@ static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-  { MODKEY,                       XK_e,      spawn,          {.v = emacs } },
-  { MODKEY|ShiftMask,             XK_e,      spawn,          {.v = explorer } },
-  { MODKEY,                       XK_b,      spawn,          {.v = browser } },
+    { MODKEY,                       XK_e,      spawn,          {.v = emacs } },
+    { MODKEY|ShiftMask,             XK_e,      spawn,          {.v = explorer } },
+    { MODKEY,                       XK_b,      spawn,          {.v = browser } },
 	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
